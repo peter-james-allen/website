b350fb0c4fef585ca760abea3119b12b399cde59
Pete <peter_james_allen@hotmail.com>
2021-06-10 17:46:34 +0100

    changed to POST request

--- assets/js/form.js
+++ assets/js/form.js
@@ -4,7 +4,7 @@ const form = document.getElementById("submitRSVP");
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form),
-  mode: "no-cors"}, headers: {"content-type": "multipart/form-data"})
+  mode: "no-cors", headers: {"content-type": "multipart/form-data"})
   .then(response => console.log('Success!' + body, response))
   .catch(error => console.error('Error!', error.message))
 })
