cde79b0c544fe5d9ca0a3e70c218815b9ef64179
Pete <peter_james_allen@hotmail.com>
2021-06-10 17:34:58 +0100

    changed to GET request

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,9 +1,9 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzQTvCSkH7nY55HSTMhE9rGOB0NJKhLfGbagOcO24oEMdGU9NG4jZ5ohSfqay3gV8zsjQ/exec'
+const scriptURL = "https://script.google.com/macros/s/AKfycbzQTvCSkH7nY55HSTMhE9rGOB0NJKhLfGbagOcO24oEMdGU9NG4jZ5ohSfqay3gV8zsjQ/exec"
 const form = document.getElementById("submitRSVP");
 
 form.addEventListener('submit', e => {
   e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form),
+  fetch(scriptURL, { method: 'GET', body: new FormData(form),
   headers: {'Access-Control-Allow-Origin': '*'}})
   .then(response => console.log('Success!' + body, response))
   .catch(error => console.error('Error!', error.message))
